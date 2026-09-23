78b74d509fe64de621d5b2df97081175e87efe5c
saarthak-hive <saarthak.gupta@hiveapi.com>
2026-09-23 15:55:06 +0530

    Text flow and scroll animation

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-gsap.to(
-    "#box", {
-         x: 1000,
-         duration: 2,
-         delay: 1,
-         backgroundColor: 'blue',
-         borderRadius: 50
-    }
-)
+gsap.to("#box", {
+  x: 1000,
+  duration: 2,
+  delay: 1,
+  backgroundColor: "blue",
+  borderRadius: 50,
+});
 // .from(
 //     "#box", {
 //          x: 1000,
@@ -17,7 +15,6 @@ gsap.to(
 //     }
 // )
 
-
 // gsap.to(
 //     ".box-2", {
 //          x: 500,
@@ -26,17 +23,13 @@ gsap.to(
 //          delay: 1,
 //     }
 // )
-gsap.from(
-    ".box-2", {
-         x: 1000,
-         y: -300,
-         duration: 2,
-         delay: 1,
-         rotate: 90,
-    }
-)
-
-
+gsap.from(".box-2", {
+  x: 1000,
+  y: -300,
+  duration: 2,
+  delay: 1,
+  rotate: 90,
+});
 
 // gsap.from("h1", {
 //     color: "red",
@@ -53,30 +46,27 @@ gsap.from(
 const tl = gsap.timeline();
 
 tl.to("#timeline-box-1", {
-    x: 1000,
-    rotate: 360,
-    duration: 1,
-    delay:2
-})
+  x: 1000,
+  rotate: 360,
+  duration: 1,
+  delay: 2,
+});
 
 tl.to("#timeline-box-2", {
-    x: 1000,
-    duration: 3
-})
+  x: 1000,
+  duration: 3,
+});
 tl.to("#timeline-box-3", {
-    x: 1000,
-    duration: 2
-})
+  x: 1000,
+  duration: 2,
+});
 
-
-
-
-gsap.from( "#page-1 #page-box", {
-    rotate: 360,
-    scale: 0,
-    delay: 1,
-    duration: 2,
-})
+gsap.from("#page-1 #page-box", {
+  rotate: 360,
+  scale: 0,
+  delay: 1,
+  duration: 2,
+});
 
 // gsap.from( "#page-2 #page-box", {
 //     rotate: 360,
@@ -91,22 +81,21 @@ gsap.from( "#page-1 #page-box", {
 //     }
 // })
 
-
 gsap.from("#page-2 #page-box", {
-    opacity: 0,
-    scale: 0,
-    rotate: 720,
-    duration: 1,
-    scrollTrigger: {
-        trigger: "#page-2 #page-box",
-        scroller: "body",
-        // markers: true,
-        start: "top 50%",
-        // end: "top 30%",
-        scrub: true,
-        pin: true,
-    }
-})
+  opacity: 0,
+  scale: 0,
+  rotate: 720,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#page-2 #page-box",
+    scroller: "body",
+    // markers: true,
+    start: "top 50%",
+    // end: "top 30%",
+    scrub: true,
+    pin: true,
+  },
+});
 
 // gsap.from( "#page-1 #page-box", {
 //     rotate: 360,
@@ -116,20 +105,17 @@ gsap.from("#page-2 #page-box", {
 // })
 
 gsap.to("#h-scroll-2 h1", {
-    transform: "translate(-200%)",
-    scrollTrigger: {
-        trigger: "#h-scroll-2",
-        scroller: "body",
-        scrub: true,
-        pin: true,
-        markers: true,
-        start: "top 0%",
-        end: "top -100%",
-    }
-})
-
-
-
+  transform: "translate(-200%)",
+  scrollTrigger: {
+    trigger: "#h-scroll-2",
+    scroller: "body",
+    scrub: true,
+    pin: true,
+    markers: true,
+    start: "top 0%",
+    end: "top -100%",
+  },
+});
 
 // const initialPath = "M 0 0 Q 150 0 300 0";
 
@@ -158,9 +144,6 @@ gsap.to("#h-scroll-2 h1", {
 //     })
 // })
 
-
-
-
 // const cursor = document.querySelector("#cursor");
 // console.log(document);
 // console.log(cursor);
@@ -172,7 +155,6 @@ gsap.to("#h-scroll-2 h1", {
 //         duration: 0.1,
 //     })
 // })
-
 
 // const image = document.querySelector("#image");
 
@@ -189,33 +171,88 @@ gsap.to("#h-scroll-2 h1", {
 //     })
 // })
 
+// const tln = gsap.timeline({ paused: true });
+
+// tln.to("#full", {
+//   right: 0,
+//   // duration: 1,
+// });
+
+// tln.from("#full h4", {
+//   x: 100,
+//   opacity: 0,
+//   // duration: 1,
+//   // stagger: 1
+// });
+
+// tln.from("#full #ic", {
+//   opacity: 0,
+// });
+
+// const openBtn = document.querySelector("#i.open").addEventListener("click", () => {
+//     tln.play();
+// })
+// // console.log(document.querySelector("#full #ic.close"))
+// const closeBtn = document.querySelector("#full #ic.close").addEventListener("click", () => {
+//     console.log("first")
+//     tln.reverse();
+// })
+
+// const h1Ele = document.querySelector("h1");
+// const h1 = h1Ele.textContent;
+
+// const splittedText = h1.split("");
+
+// let clutter = "";
+// h1.split(" ").forEach((word) => {
+//     const length = Math.floor(word.length / 2);
+//   word.split("").forEach((char, index) => {
+//     clutter += `<span class=${index < length ? "f" : "s"}>${char}</span>`;
+//   });
+//   clutter += ` `
+// });
+
+// h1Ele.innerHTML = clutter;
+
+// gsap.from("h1 span.f ", {
+//   y: 100,
+//   duration: 1,
+//   stagger: 0.3,
+//   opacity: 0,
+// });
+// gsap.from("h1 span.s ", {
+//   y: 100,
+//   duration: 1,
+//   stagger: -0.3,
+//   opacity: 0,
+// });
 
 
 
-const tln = gsap.timeline({ paused: true });
-
-tln.to("#full", {
-    right: 0,
-    // duration: 1,
-})
-
-tln.from("#full h4", {
-    x: 100,
-    opacity: 0,
-    // duration: 1,
-    // stagger: 1
-})
-
-tln.from("#full #ic", {
-    opacity: 0
-})
-
-
-const openBtn = document.querySelector("#i.open").addEventListener("click", () => {
-    tln.play();
-})
-console.log(document.querySelector("#full #ic.close"))
-const closeBtn = document.querySelector("#full #ic.close").addEventListener("click", () => {
-    console.log("first")
-    tln.reverse();
+window.addEventListener("wheel", (e) => {
+    if(e.deltaY > 0){
+        gsap.to(
+            ".marque", {
+                transform: "translateX(0%)",
+                duration: 2,
+                repeat: -1,
+                ease: "none",
+            }
+        )
+        gsap.to(".marque span", {
+            rotate: 0
+        })
+    }else{
+        gsap.to(
+            ".marque", {
+                transform: "translateX(-200%)",
+                duration: 2,
+                repeat: -1,
+                ease: "none",
+            }
+        )
+        gsap.to(".marque span", {
+            rotate: 180
+        })
+    }
 })
